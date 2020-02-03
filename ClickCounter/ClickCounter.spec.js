f257a75fd62102e4f4c3a02d0bfddad9cadf87df
ymchoi <ymchoi@wemb.co.kr>
2020-02-03 13:46:16 +0900

    ClickCounter 모듈의 increase()는 카운터 값을 1만큼 증가한다.

--- ClickCounter/ClickCounter.spec.js
+++ ClickCounter/ClickCounter.spec.js
@@ -1,14 +1,22 @@
 describe('App.ClickCounter', ()=> {
+	let counter
+	beforeEach(()=>{
+		counter = App.ClickCounter()
+	})
+
   describe('getValue()', ()=> {
     it('초기값이 0인 카운터 값을 반환한다', ()=> {
-      const counter = App.ClickCounter()
       expect(counter.getValue()).toBe(0)
     })
   })
 
   describe('increase()', ()=> {
     it('카운터를 1 올린다', ()=> {
-      todo 
+		  const initialValue = counter.getValue();
+			//실행
+      counter.increase()
+      //단언
+      expect(counter.getValue()).toBe(initialValue + 1)
     })
   })
 })
