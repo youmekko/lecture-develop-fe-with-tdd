c30ff0965645e6d541a4bd8c0fad8a6cf1525355
ymchoi <ymchoi@wemb.co.kr>
2020-02-03 14:48:05 +0900

    clickCounter를 주입하지 않으면 에러를 던진다

--- ClickCounter/ClickCountView.js
+++ ClickCounter/ClickCountView.js
@@ -1,6 +1,8 @@
 var App = App || {}
 
 App.ClickCountView = ((clickCounter, updateElement) => {
+    if(!clickCounter) throw Error('clickCounter')
+
     return {
         updateView(){
             updateElement.innerHTML = clickCounter.getValue()
--- ClickCounter/ClickCountView.spec.js
+++ ClickCounter/ClickCountView.spec.js
@@ -1,14 +1,23 @@
 describe('App.ClickCountView', ()=> {
+  let clickCounter, updateElement, view
+  beforeEach(()=>{
+    clickCounter = App.ClickCounter()
+    updateElement = document.createElement('span')
+    view = App.ClickCountView(clickCounter, updateElement)
+  })
+  
+  it('clickCounter를 주입하지 않으면 에러를 던진다', ()=>{
+    const clickCounter = null
+    const updateElement = document.createElement('span')
+    const actual = () => App.ClickCountView(clickCounter, updateElement)
+    expect(actual).toThrowError()
+  })
+  
+  it('updateElement를 주입하지 않으면 에러를 던진다', ()=>{
+    
+  })
+  
   describe('updateView()', ()=> {
-
-    let clickCounter, updateElement, view
-
-    beforeEach(()=>{
-      clickCounter = App.ClickCounter()
-      updateElement = document.createElement('span')
-      view = App.ClickCountView(clickCounter, updateElement)
-    })
-
     it('ClickCounter의 getValue() 값을 출력한다', ()=> {
       const counterValue = clickCounter.getValue()
       view.updateView()
